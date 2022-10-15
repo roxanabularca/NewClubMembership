2be9eee8f6b84b3335154da978007d95d1bae120
roxanabularca <bularcaroxana@gmail.com>
2022-10-15 13:12:22 +0300

    second commit

---
+++ NewClubMembership/NewClubMembership/Data/Migrations/20221015101008_NewClubMembers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewClubMembership.Data.Migrations
+{
+    public partial class NewClubMembers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
